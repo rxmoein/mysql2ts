039825d523d2f078dbbc2b29b77ae8f9c5940c7a
rxmoein <rxmoein@gmail.com>
2020-06-11 11:01:35 +0430

    Added execution time

--- src/services/generator-service.ts
+++ src/services/generator-service.ts
@@ -5,6 +5,7 @@ import { injectable, inject } from 'inversify';
 import { MysqlService } from './mysql-service';
 import { ToolsService } from './tools-service';
 import { Table } from '../models/table';
+import chalk = require('chalk');
 
 @injectable()
 export class GeneratorService {
@@ -26,15 +27,22 @@ export class GeneratorService {
     }
 
     async generate() {
+        const start = +new Date()
         const config = this.configService.getConfig();
 
         await this.toolsService.initializeMysql();
         await this.toolsService.useDatabase(config.DatabaseName);
         const tables = await this.toolsService.getTables();
         await this.exportTables(tables);
+
+        const end = +new Date() - start;
+        console.log(chalk.green(`Models generated successfully in ${end}ms`));
+        process.exit(0);
     }
 
     async exportTables(tables: Table[]) {
+
+
         const config = this.configService.getConfig();
 
         const outputDirectory = process.cwd() + '/' + config.OutputDirectory;
@@ -45,7 +53,5 @@ export class GeneratorService {
         for (const table of tables) {
             writeFileSync(`${outputDirectory}/${table.name}.model.ts`, table.getClassDefinitionString());
         }
-
-        process.exit(0);
     }
 }
